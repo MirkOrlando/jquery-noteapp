40d115c8990f4253297ee486f225f668174989e3
MirkOrlando <mirko@orlando-m.it>
2022-08-19 19:03:26 +0200

    print all notes in #notes_list

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,5 +22,51 @@ function getAllNotes() {
 }
 
 function printListNotes(data) {
-    console.log(data);
+    //Handlebars
+    const listYearsContainer = $('#notes_list .notes_list_main .notes_container');
+    const source = $('#note-year').html();
+    const template = Handlebars.compile(source)
+
+    //get available years
+    const years = [];
+    data.forEach(note => {
+        const year = dayjs(note.updatedAt).format('YYYY');
+        if (!years.includes(year)) {
+            years.push(year)
+        }
+    });
+
+    //print for years
+    years.forEach(function (year) {
+        const html = template({ year });
+        listYearsContainer.append(html)
+
+        const notesFiltered = data.filter(function (note) {
+            return dayjs(note.updatedAt).format('YYYY')
+                === year;
+        })
+
+        printNotes(notesFiltered, year)
+    })
+
+    activateFirstNote()
+}
+
+function printNotes(data, year) {
+    //console.log(data, year);
+    const listNotesContainer = $('.year' + year + ' ' + '.notes-menu__list');
+
+    const source = $('#note-list-template').html();
+    const template = Handlebars.compile(source);
+
+    data.forEach((note) => {
+        /* const fakeElement = $('<div></div>').append(note.text)
+        console.log(note);
+        console.log(fakeElement); */
+        const text = note.text.slice(0, 30) + '...'
+        //console.log(text);
+        const context = { id: note.id, title: note.title, text, updatedAt: dayjs(note.updatedAt).format('DD MMMM YYYY') };
+        const html = template(context);
+        listNotesContainer.append(html)
+    })
 }
